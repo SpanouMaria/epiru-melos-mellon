8c44032c098f95d7962fcd93e38259bea9557869
Spanou Maria <spanoumaria03@gmail.com>
2026-04-20 22:11:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const btn = document.getElementById('audioBtn');
 const modal = document.getElementById('audioModal');
+const closeBtn = document.querySelector('.close');
 
 btn.addEventListener('click', () => {
-  modal.style.display = (modal.style.display === 'block') ? 'none' : 'block';
+    modal.style.display = (modal.style.display === 'block') ? 'none' : 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 });
